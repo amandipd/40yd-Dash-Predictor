ce1dea92ec62c92b01239807b3a54c795f24fcd0
Amandip Dutta <amandip@dutta@gmail.com>
2025-01-07 10:55:37 -0800

    update project idea

--- algs.py
+++ algs.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn import datasets
 
+def organize_data(df):
+    num_rows = len(df)
+        
 
 def main():
     print("hello")
-    df = pd.read_csv('combine_data_2000-2018.csv')
-    print(df)
+    df = pd.read_csv('cybersecurity_attacks.csv')
+    clean_data(df)
 
 
 if __name__ == "__main__":
