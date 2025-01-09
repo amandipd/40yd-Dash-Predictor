bedb4cb6c1a26db59776dcd43524476f2afa4906
Amandip Dutta <amandip@dutta@gmail.com>
2025-01-08 22:43:11 -0800

    clean data

--- algs.py
+++ algs.py
@@ -2,12 +2,27 @@ import pandas as pd
 import numpy as np
 from sklearn import datasets
 
-def organize_data(df):
-    num_rows = len(df)
+'''
+Keep only input features:
+Source IP Address, Destination IP Address, Source Port, Destination Port, Protocol, Packet Length, Traffic Type,
+Anomaly Scores, Payload Data
+and target variable:
+Attack Type
+'''
+def clean_data(df):  
+    dropped_columns = [
+        "Timestamp", "Packet Type", "Payload Data", "Malware Indicators", 
+        "Alerts/Warnings", "Attack Signature", "Action Taken", "Severity Level", 
+        "User Information", "Network Segment", "Geo-location Data", 
+        "Proxy Information", "Firewall Logs", "IDS/IPS Alerts", "Log Source"
+    ]  
+    df.drop(dropped_columns, axis = 1, inplace = True)
+    print(df.columns)
+    
+
         
 
 def main():
-    print("hello")
     df = pd.read_csv('cybersecurity_attacks.csv')
     clean_data(df)
 
