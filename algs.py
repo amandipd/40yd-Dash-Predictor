17be5d5c293ce1b040f85ab2c869dc949570700d
Amandip Dutta <amandip@dutta@gmail.com>
2025-01-08 23:19:19 -0800

    normalize data

--- algs.py
+++ algs.py
@@ -2,13 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn import datasets
 
-'''
-Keep only input features:
-Source IP Address, Destination IP Address, Source Port, Destination Port, Protocol, Packet Length, Traffic Type,
-Anomaly Scores, Payload Data
-and target variable:
-Attack Type
-'''
+def normalize_cols(df, colName):
+    mean = df[colName].mean()
+    std_dev = df[colName].std()
+
+
+
 def clean_data(df):  
     dropped_columns = [
         "Timestamp", "Packet Type", "Payload Data", "Malware Indicators", 
@@ -17,10 +16,13 @@ def clean_data(df):
         "Proxy Information", "Firewall Logs", "IDS/IPS Alerts", "Log Source"
     ]  
     df.drop(dropped_columns, axis = 1, inplace = True)
-    print(df.columns)
-    
 
-        
+    # Normalizing Packet Length and Anomaly Score with z-score standardization
+    col1 = "Packet Length"
+    col2 = "Anomaly Score"
+    df[col1] = (df[col1] - df[col1].mean()) / df[col1].std()
+    df[col2] = (df[col1] - df[col1].mean()) / df[col1].std()
+
 
 def main():
     df = pd.read_csv('cybersecurity_attacks.csv')
